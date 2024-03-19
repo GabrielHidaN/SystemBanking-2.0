c4a18a985290052a8fd0683bc260dc63666b8ccf
Gabriel Victor <122997003+GabrielHidaN@users.noreply.github.com>
2024-03-18 22:26:05 -0300

    first commit

---
+++ index.py
@@ -0,0 +1,21 @@
+#dunder
+__version__ = "0.1.0"
+__author__ = "Gabriel Victor"
+__license__ = "Unlincese"
+
+import os
+import datetime
+
+
+#Menu do sistema
+menu ='''
+
+BANKING SYSTEM
+
+[d] Deposito
+[s] Saque
+[e] Extrato
+[q] Sair
+
+=>
+'''
